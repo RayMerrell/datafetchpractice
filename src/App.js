350bc3d5a5367ae3b852bf7db0ca9bac496c6f4e
Ray Merrell <RayMerrell@outlook.com>
2023-04-04 11:34:36 +0100

    god I HATE react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react"; 
 import './App.css';
 
 function App() {
+  const [report, setReport]=useState({description:null});
+  const [city, setCity]=useState("warrington");
+  const [errMsg, setErrMsg]=useState(null);
+  const [strReturn, setstrReturn]=useState("");
+
+  const handleEdit=(e)=>{
+    console.log("edit");
+    setCity(e.target.value);
+  };
+
+  const fetchReport = async () =>{
+    let strRequestedTown = city;
+    if (!strRequestedTown == null || !strRequestedTown ==""){
+      console.log("Requsted Town", strRequestedTown);
+      let strRequest = "https://goweather.herokuapp.com/weather/" + strRequestedTown
+      try{
+        const response = await fetch(strRequest);
+        if (!response.ok){
+          throw new Error(response.statusText);
+        }
+        const weatherData =  await response.json();
+        console.log(weatherData);
+        setReport(weatherData);
+        console.log("shit",report.description);
+        if (report.description === "" || report.description === null){
+          console.log("No Data section");
+          setstrReturn("Could not find any data for "+ city);
+        }else{
+          console.log("data section");
+          setstrReturn("The weather for " + city + " is " + report.description);
+        } 
+      }catch(err){
+        console.log("Error:", err);
+        setErrMsg("Data not found");
+      }
+    };
+  }  
+  useEffect(() => {
+    console.log("fetch");
+    fetchReport();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Forecast</h1>
+      <input type="text" id="textContent" key="textInput" onChange={handleEdit} value={city}>
+      </input><input type="submit" value="submit" onClick={fetchReport}></input>
+      <p>{strReturn}</p>
     </div>
   );
 }
